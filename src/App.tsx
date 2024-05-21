3551c2881336e5a384de1b5b23f754ece3a5c2b1
unknown <antnamve@gmail.com>
2024-05-21 23:40:01 +0300

    7. useTheme, themeContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from "react-router-dom";
-import "./index.scss";
+import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense } from "react";
+import { Suspense, useContext, useState } from "react";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
       <Suspense fallback={<div>Loading...</div>}>
