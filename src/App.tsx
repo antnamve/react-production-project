849b6d2a20ebb42ed9d915983cc18b957d8291ba
unknown <antnamve@gmail.com>
2024-05-22 10:34:50 +0300

    8. classnames

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,15 @@ import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense, useContext, useState } from "react";
+import { Suspense } from "react";
 import { useTheme } from "./theme/useTheme";
+import { classNames } from "./helpers/classnames/classnames";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
 
   return (
-    <div className={`app ${theme}`}>
+    <div className={classNames("app", {}, [theme])}>
       <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
