c11260e245155f774308a08aadafd13e00a19d56
unknown <antnamve@gmail.com>
2024-05-22 18:02:36 +0300

    11. AppRouter

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import "./styles/index.scss";
 import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { useTheme } from "./providers/ThemeProvider";
 import { classNames } from "shared/lib/classnames/classnames";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/routes";
 
 export const App = () => {
   const { theme, toggleTheme } = useTheme();
@@ -15,12 +12,7 @@ export const App = () => {
       <button onClick={toggleTheme}>Toggle</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О нас</Link>
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path={"/about"} element={<AboutPage />} />
-          <Route path={"/"} element={<MainPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter />
     </div>
   );
 };
